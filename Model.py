f6f1592f6b0693f45a332c46e12d2cb5999c338e
kond1ter <61204648+kond1ter@users.noreply.github.com>
2023-03-11 00:17:41 +0300

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,75 @@
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+layers = tf.keras.layers
+
+
+class Model:
+    def __init__(self, cnn_model, image_size, batch_size, classes):
+        self.model = tf.keras.models.Sequential()
+        self.cnn_model = cnn_model
+        self.image_size = image_size
+        self.batch_size = batch_size
+        self.classes = classes
+
+    def refit(self, name, load_name, data, epochs, optimise=False, show_history=False):
+        train_ds, validate_ds = self.prepare_ds(data, optimise)
+        self.cnn_model = tf.keras.models.load_model(load_name)
+        history = self.cnn_model.fit(train_ds, validation_data=validate_ds, epochs=epochs)
+        if show_history: self.show_history_plots(history, epochs)
+        self.cnn_model.save(name)
+
+    def generate(self, name, data, epochs, optimise=False, show_history=False):
+        train_ds, validate_ds = self.prepare_ds(data, optimise)
+        self.cnn_model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
+        history = self.cnn_model.fit(train_ds, validation_data=validate_ds, epochs=epochs)
+        if show_history: self.show_history_plots(history, epochs)
+        self.cnn_model.save(name)
+
+    def load(self, name):
+        model = tf.keras.models.load_model(name)
+        self.model = model
+
+    def test(self, img):
+        img = tf.keras.utils.load_img(img, target_size=(self.image_size, self.image_size))
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array / self.image_size, 0)
+        predictions = self.model.predict(img_array, verbose=0)
+        score = tf.nn.softmax(predictions[0])
+        answer = self.classes[np.argmax(score)]
+        return answer
+
+    def prepare_ds(self, data, optimise):
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+            data, validation_split=0.2, subset="training", seed=321, batch_size=self.batch_size,
+            image_size=(self.image_size, self.image_size))
+        validate_ds = tf.keras.utils.image_dataset_from_directory(
+            data, validation_split=0.2, subset="validation", seed=321, batch_size=self.batch_size,
+            image_size=(self.image_size, self.image_size))
+
+        if optimise:
+            autotune = tf.data.AUTOTUNE
+            train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=autotune)
+            validate_ds = validate_ds.cache().prefetch(buffer_size=autotune)
+
+        train_ds = train_ds.map(lambda x, y: (x / self.image_size, y))
+        validate_ds = validate_ds.map(lambda x, y: (x / self.image_size, y))
+        return train_ds, validate_ds
+
+    def show_history_plots(self, history, epochs):
+        acc = history.history["accuracy"]
+        val_acc = history.history["val_accuracy"]
+        loss = history.history["loss"]
+        val_loss = history.history["val_loss"]
+        epochs_range = range(epochs)
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc)
+        plt.plot(epochs_range, val_acc)
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss)
+        plt.plot(epochs_range, val_loss)
+        plt.show()
+
---
+++ main.py
@@ -0,0 +1,49 @@
+
+import os
+from Model import *
+
+image_size = 224
+batch_size = 32
+dataset_path = "train_small"
+classes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+           "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+data_augmentation = tf.keras.models.Sequential([
+    layers.RandomFlip("horizontal", seed=321, input_shape=(image_size, image_size, 3)),
+    layers.RandomRotation(0.01, seed=321, fill_mode="reflect"),
+    layers.RandomContrast(0.3, seed=321),
+    # layers.RandomBrightness(0.3, seed=321),
+    # layers.RandomCrop(240, 240, seed=321),
+])
+cnn_model = tf.keras.models.Sequential([
+    data_augmentation,
+    layers.Conv2D(16, (3, 3), padding="same", activation='relu'),
+    layers.MaxPooling2D((2, 2), strides=2),
+    layers.Conv2D(16, (3, 3), padding="same", activation='relu'),
+    layers.MaxPooling2D((2, 2), strides=2),
+    layers.Conv2D(32, (3, 3), padding="same", activation='relu'),
+    layers.MaxPooling2D((2, 2), strides=2),
+    layers.Conv2D(32, (3, 3), padding="same", activation='relu'),
+    layers.MaxPooling2D((2, 2), strides=2),
+    layers.Conv2D(64, (3, 3), padding="same", activation='relu'),
+    layers.MaxPooling2D((2, 2), strides=2),
+    layers.Dropout(0.3),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(len(classes), activation="softmax")
+])
+
+model = Model(cnn_model, image_size, batch_size, classes)
+model.generate(name="sl_classification_1", data=dataset_path,
+    epochs=10, optimise=False, show_history=True)
+
+# model.load("sl_classification_1")
+
+# model.refit("sl_classification_1", "sl_classification_1",
+#     "archive/asl_alphabet_train/asl_alphabet_train",
+#     epochs=20, optimise=False, show_history=True)
+
+# for file in os.listdir("test"):
+#     answer = model.test(f"test/{file}")
+#     print(f"predict {answer} on {file}")
+
